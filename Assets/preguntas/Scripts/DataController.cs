86d97f4f33a57b095c69f47418c3535f79960438
suldreck <suldreck@hotmail.com>
2018-06-12 22:56:48 +0200

    con bases de dato y a lo loco((las cuatro fichas dan problemas aun=
    pues eso, seguimos con el problema del foco.

--- Assets/preguntas/Scripts/DataController.cs
+++ Assets/preguntas/Scripts/DataController.cs
@@ -1,20 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using System.IO;
 
 public class DataController : MonoBehaviour 
 {
-	public RoundData[] allRoundData;
-	
-	void Start ()  
+	private  RoundData[] allRoundData;
+    private string gameDataFileName = "Data.json";
+    void Start ()  
 	{
 		DontDestroyOnLoad (gameObject);
-		
-		SceneManager.LoadScene ("menu");
+        LoadGame();
+        SceneManager.LoadScene ("menu");
 	}
 	
 	public RoundData GetCurrentRoundData()
 	{
 		return allRoundData [0];
 	}
+    private void LoadGame()
+    {
+        string filePath = Path.Combine(Application.streamingAssetsPath, gameDataFileName);
+        if (File.Exists(filePath))
+        {
+            string dataAsJson = File.ReadAllText(filePath);
+            GameData loadedData = JsonUtility.FromJson<GameData>(dataAsJson);
+            allRoundData = loadedData.allRoundData;
+        }
+        else
+        {
+            Debug.LogError("No se puede cargar las preguntas");
+        }
+    }
 }
