cda585e780b4bd484b6373ba32e6c1e1570cd9ef
suldreck <suldreck@hotmail.com>
2018-06-13 17:57:54 +0200

    Tablero cambiado (más tablero que nunca)

--- Assets/preguntas/Scripts/DataController.cs
+++ Assets/preguntas/Scripts/DataController.cs
@@ -6,7 +6,7 @@ using System.IO;
 public class DataController : MonoBehaviour 
 {
 	private  RoundData[] allRoundData;
-    private string gameDataFileName = "Data.json";
+    private string gameDataFileName = "QuestionsAndAnswers.json";
     void Start ()  
 	{
 		DontDestroyOnLoad (gameObject);
@@ -18,18 +18,23 @@ public class DataController : MonoBehaviour
 	{
 		return allRoundData [0];
 	}
+
+
     private void LoadGame()
     {
         string filePath = Path.Combine(Application.streamingAssetsPath, gameDataFileName);
         string filePath2 = Path.Combine(Application.streamingAssetsPath, "");
-        if (File.Exists(filePath) || File.Exists(filePath2+"/"+gameDataFileName))
+        if (File.Exists(filePath))
         {
             string dataAsJson = File.ReadAllText(filePath);
             GameData loadedData = JsonUtility.FromJson<GameData>(dataAsJson);
             allRoundData = loadedData.allRoundData;
         }
-        else
+        else if (File.Exists(filePath2 + "/" + gameDataFileName))
         {
+            string dataAsJson = File.ReadAllText(filePath2 + "/" + gameDataFileName);
+            GameData loadedData = JsonUtility.FromJson<GameData>(dataAsJson);
+            allRoundData = loadedData.allRoundData;
             Debug.LogError("No se puede cargar las preguntas");
         }
     }
